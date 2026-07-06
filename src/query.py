be923d986b20be32d864c31d90790f11bf5e8e15
Lawjay Lee <lawjaylee1@icloud.com>
2026-07-06 00:19:25 -0700

    query

---
+++ src/query.py
@@ -0,0 +1,23 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "numpy>=2.5.1",
+#     "pandas>=3.0.3",
+# ]
+# ///
+
+from pathlib import Path
+import pandas as pd
+
+
+CSV_PATH = Path(__file__).parents[1] / "data" / "data.csv"
+df = pd.read_csv(CSV_PATH)
+
+df = df.query("gap > 0.3 and high > 1")
+
+df = df.sort_values(["date", "gap"], ascending=False)
+df = df.reset_index(drop=True)
+
+df = df[["date", "ticker", "gap", "opchange"]]
+
+print(df)

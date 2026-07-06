ce1e6ea310e6b22692a29018409f582144d6c1cb
Lawjay Lee <lawjaylee1@icloud.com>
2026-07-06 00:21:00 -0700

    better date

--- src/query.py
+++ src/query.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 CSV_PATH = Path(__file__).parents[1] / "data" / "data.csv"
 df = pd.read_csv(CSV_PATH)
+df["date"] = pd.to_datetime(df["date"])
 
 df = df.query("gap > 0.3 and high > 1")
 
@@ -19,5 +20,6 @@ df = df.sort_values(["date", "gap"], ascending=False)
 df = df.reset_index(drop=True)
 
 df = df[["date", "ticker", "gap", "opchange"]]
+df["date"] = df["date"].dt.strftime("%Y-%m-%d")
 
 print(df)
